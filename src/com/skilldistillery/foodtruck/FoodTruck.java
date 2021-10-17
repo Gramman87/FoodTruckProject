d9372e582cfa05c572b5a8f83522be5d2b7fda69
Gramman87 <gramman87@gmail.com>
2021-10-16 18:38:30 -0600

    working commit

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -1,5 +1,50 @@
 package com.skilldistillery.foodtruck;
 
 public class FoodTruck {
+	private static int foodTruckId = 0000;
+	private String foodTruckName;
+	private String foodType;
+	private int TruckRating;
+	
+	public FoodTruck(String foodTruckName, String foodType, int truckRating) {
+		this.foodTruckId++;
+		this.foodTruckName = foodTruckName;
+		this.foodType = foodType;
+		TruckRating = truckRating;
+	}
+
+	public static int getFoodTruckId() {
+		return foodTruckId;
+	}
+
+	public static void setFoodTruckId(int foodTruckId) {
+		FoodTruck.foodTruckId = foodTruckId;
+	}
+
+	public String getFoodTruckName() {
+		return foodTruckName;
+	}
+
+	public void setFoodTruckName(String foodTruckName) {
+		this.foodTruckName = foodTruckName;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public int getTruckRating() {
+		return TruckRating;
+	}
+
+	public void setTruckRating(int truckRating) {
+		TruckRating = truckRating;
+	}
+	
+	
 
 }
--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -1,10 +1,56 @@
 package com.skilldistillery.foodtruck;
 
+import java.util.Scanner;
+
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
 		//Scanner, menu
+		Scanner sc = new Scanner(System.in);
+		boolean voting = true;
+		int userIput = 0;
+		
+		String foodTruckName = "";
+		String foodType = "";
+		int truckRating = 0;
+		
+		System.out.println("Welcome to the Food Truck Challenge!");
+				
+		while(voting) {
+			switch (userIput) {
+			case 1:
+				System.out.println("Please enter the following information.");
+				System.out.print("Food truck name: ");
+				foodTruckName = sc.nextLine();
+				
+				System.out.print("Food type: ");
+				foodType = sc.nextLine();
+				
+				System.out.print("Rating: ");
+				truckRating = sc.nextInt();
+				
+				FoodTruck(foodTruckName, foodType, truckRating);
+				break;
 
+			default:
+				break;
+			}
+		}
+		
+		sc.close();
+
+	}
+	
+	public void printMenu() {
+		System.out.println();
+		System.out.println("========= MENU =========");
+		System.out.println("|                      |");
+		System.out.println("|       1.Voting       |");
+		System.out.println("|                      |");
+		System.out.println("|       2.Exit         |");
+		System.out.println("|                      |");
+		System.out.println("========================");
+		System.out.println();
 	}
 
 }
